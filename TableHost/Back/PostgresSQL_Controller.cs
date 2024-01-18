a67e6040a114e3c85b5e9b706339d017cb0c46e0
Detcl <Black.File@yandex.ru>
2024-01-18 15:59:01 +0500

    Добавьте файлы проекта.

---
+++ TableHost/Back/PostgresSQL_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableHost.Back
+{
+    internal class PostgresSQL_Controller
+    {
+    }
+}
---
+++ TableHost/UserControls/Main_UC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TableHost.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Main_UC.xaml
+    /// </summary>
+    public partial class Main_UC : UserControl
+    {
+        public Main_UC()
+        {
+            InitializeComponent();
+        }
+    }
+}
